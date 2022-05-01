8d3552d8cb1940ba7877e08b1380275247537fa5
Spencer Miller <wspencer.miller@gmail.com>
2022-05-01 01:28:19 -0500

    freedcodecamp - serve static

--- myApp.js
+++ myApp.js
@@ -3,8 +3,9 @@ var app = express(
    
    );
    app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(__dirname + "/views/index.html");
   })
+  app.use('/public', express.static(__dirname + "/public"));
 
 
 
