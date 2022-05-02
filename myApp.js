285c38c2d25461db4c6c719856122a7e9566320c
Spencer Miller <wspencer.miller@gmail.com>
2022-05-02 14:40:38 -0500

    adding

--- myApp.js
+++ myApp.js
@@ -1,16 +1,53 @@
+require('dotenv').config();
+var bodyParser = require('body-parser')
 var express = require('express');
+
 var app = express(
    
    );
-   app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html");
-  })
-  app.use('/public', express.static(__dirname + "/public"));
+   app.use(bodyParser.urlencoded({ extended: false }));
+   app.use(bodyParser.json());
+   
+   app.use( (req, res, next)=>{
+    var string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string);
+    next();
+    })
+   app.get("/json", function(req, res) {
+    let message = 'Hello json'
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+      return res.status(200).json({"message": message.toUpperCase()})
+    }
+    return res.status(200).json({"message": message})
+  });
+  const middleware = (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  };
 
+  app.get("/name", function(req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    // OR you can destructure and rename the keys
+    var { first: firstName, last: lastName } = req.query;
+    // Use template literals to form a formatted string
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+  });
 
-
-
-
+  app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json({
+      echo: word
+    });
+  });
+  
+  app.get("/now", middleware, (req, res) => {
+    res.send({
+      time: req.time
+    });
+  });
 
 
 
